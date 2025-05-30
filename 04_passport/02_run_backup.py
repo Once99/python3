6db6448bace31f9f28dea715f35ec61bb78b6de2
loki <webm@dc66.net>
2025-05-30 15:55:56 +0800

    update

--- 04_passport/02_run_backup.py
+++ 04_passport/02_run_backup.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from pync import Notifier  # 用來觸發 macOS 通知
 
 # 設定來源與目的地
-SRC_DISK = "/Volumes/My Passport/"
-DST_DISK = "/Volumes/SP PHD U3/"
+SRC_DISK = "/Volumes/TOSHIBA EXT/2025-05-22"
+DST_DISK = "/Volumes/TOSHIBA EXT 1/"
 
 def run_backup():
     if not os.path.exists(SRC_DISK):
@@ -15,10 +15,10 @@ def run_backup():
         Notifier.notify("目標硬碟未掛載", title="❌ 備份錯誤")
         return
 
-    # rsync 指令
+    # rsync 指令 "--delete",
     cmd = [
         "rsync",
-        "-avh", "--progress", "--delete",
+        "-avh", "--progress",
         SRC_DISK,
         DST_DISK
     ]
